a037d1714143476e19acf1b1d0f0e8d8293fab49
Cabrasky <javiermateos149@gmail.com>
2022-09-28 08:10:46 +0000

    Monitores Responsive y tarjetas

--- monitores/views.py
+++ monitores/views.py
@@ -8,7 +8,10 @@ from django.template import loader
 def grupo(request, grupo_de_edad):
     grupo = Grupo.objects.get(nombre=grupo_de_edad)
     grupos_list = Grupo.objects.order_by('id')
-    monitor_list = grupo.monis.all()
+    monitor_list = Monitor.objects.filter(grupos__nombre=grupo_de_edad)
+    for i in range(len(monitor_list)):
+        monitor_list[i].gruposList = []
+        monitor_list[i].gruposList = monitor_list[i].grupos.all()
     template = loader.get_template('monitores/main.html')
     context = {
         'sGrupo': grupo,
@@ -19,9 +22,12 @@ def grupo(request, grupo_de_edad):
 
 
 def index(request):
-    monitor_list = Monitor.objects.all
+    monitor_list = Monitor.objects.all()
     grupos_list = Grupo.objects.order_by('id')
     template = loader.get_template('monitores/main.html')
+    for i in range(len(monitor_list)):
+        monitor_list[i].gruposList = []
+        monitor_list[i].gruposList = monitor_list[i].grupos.all()
     context = {
         'grupos_list': grupos_list,
         'monitor_list': monitor_list
