2aca81af3c0dc71fe09872513eb05f369e6c1125
Cabrasky <javiermateos149@gmail.com>
2022-10-02 18:55:17 +0000

    Base de Datos Sin Colisiones

--- inicio/views.py
+++ inicio/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
+from monitores.models import Grupo
 from .models import Proyecto
+
 
 # Create your views here.
 def inicio(req):
-	proyectos = Proyecto.objects.all
+	proyectos = Proyecto.objects.all()
+	grupos_list_inicio = Grupo.objects.all()
 	return render(req, "inicio/inicio.html", {
+		'grupos_list_inicio': grupos_list_inicio,
 		'proyectos': proyectos
 	})
